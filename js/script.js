ea80eb52058ca3f04355e0000d43aa80f130d414
jfmarkan <jfmarkan@gmail.com>
2023-05-12 15:13:56 +0200

    work done!

--- js/script.js
+++ js/script.js
@@ -3,12 +3,23 @@ const passengerTour = parseInt(prompt("Inserici la distanza da percorrere (in KM
 
 let tourPrice = passengerTour * 0.21;
 
+document.getElementById("tour-price").innerHTML = tourPrice + "€";
+
 if (passengerAge < 18){
-    console.log(tourPrice * 0.8);
+    document.getElementById("passenger-age").innerHTML += 'meno di 18 anni' ;
+    document.getElementById("tour-discount").innerHTML += '20%' ;
+    let finalPrice = tourPrice * 0.8;
+    document.getElementById("final-price").innerHTML += finalPrice.toFixed(2) + '€';
 } else if (passengerAge >= 65) {
-    console.log(tourPrice * 0.6);
+    document.getElementById("passenger-age").innerHTML += 'più di 65 anni' ;
+    document.getElementById("tour-discount").innerHTML += '40%';
+    let finalPrice = tourPrice * 0.6;
+    document.getElementById("final-price").innerHTML += finalPrice.toFixed(2) + '€';
 }  else {
-    console.log(tourPrice);
+    document.getElementById("passenger-age").innerHTML = '';
+    document.getElementById("tour-discount").innerHTML = '';
+    document.getElementById("final-price").innerHTML = '';
+    document.getElementById("tour-price").innerHTML = tourPrice.toFixed(2) + "€";
 }
 
 
